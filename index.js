c8b3caae0dbf21f286a2542229791cf66bb69c3a
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-09-02 16:33:20 +0300

    add html and style the subscribe section

---
+++ index.js
@@ -0,0 +1,7 @@
+const subButton=document.getElementById("subscribe")
+subButton.addEventListener("click",function (){
+    const email=document.getElementById("email")
+    email.value(e)
+    e.target.value
+
+    })
